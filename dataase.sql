f5c5744591eba03ac8cb4d6172d2d5057f289bfd
Wahiduddin Samani <samaniwahiduddin382@gmail.com>
2024-07-16 20:27:17 +0530

    Add files via upload

---
+++ dataase.sql
@@ -0,0 +1,16 @@
+-- Database: mystudents
+CREATE TABLE `std` (
+  `sid` int(10) NOT NULL,
+  `fname` varchar(100) NOT NULL,
+  `lname` varchar(100) NOT NULL,
+  `fathername` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `gender` varchar(40) NOT NULL,
+  `ssub` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `dob` varchar(100) NOT NULL,
+  `sadd` varchar(200) NOT NULL,
+  `picture` varchar(100) NOT NULL
+)
+
